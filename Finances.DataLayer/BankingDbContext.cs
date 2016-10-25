95d83b7861d9fb3534f18b069f6d4f82d2f24758
gngaspar <ggaspar@netcabo.pt>
2016-10-25 17:46:26 +0200

    Changed Database constructor

--- Finances.DataLayer/BankingDbContext.cs
+++ Finances.DataLayer/BankingDbContext.cs
@@ -1,16 +1,32 @@
-﻿using System.Configuration;
-using Finances.DataLayer.Configurations;
-
-namespace Finances.DataLayer
+﻿namespace Finances.DataLayer
 {
     using System.Data.Entity;
+    using System.Data.Entity.SqlServer;
+    using System.IO;
+
     using Finances.Domain.Banking;
     public class BankingDbContext : DbContext
     {
+        static readonly object _lock = new object();
+
         public BankingDbContext()
             : base("BankingConnection")
         {
+            Database.SetInitializer<BankingDbContext>(null);
+            this.Configuration.AutoDetectChangesEnabled = false;
+            this.Configuration.LazyLoadingEnabled = false;
 
+            var ensureDllIsCopied = SqlProviderServices.Instance;
+
+#if DEBUG
+            this.Database.Log = s =>
+            {
+                lock (_lock)
+                {
+                    File.AppendAllText(@"C:/DBLog.txt", s);
+                }
+            };
+#endif
         }
 
         public DbSet<BankEntity> Banks
@@ -21,7 +37,10 @@ namespace Finances.DataLayer
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Configurations.Add(new BankEntityConfiguration());
+            //modelBuilder.Configurations.Add(new BankEntityConfiguration());
+
+            modelBuilder.Configurations.AddFromAssembly(this.GetType().Assembly);
+            base.OnModelCreating(modelBuilder);
         }
     }
 
