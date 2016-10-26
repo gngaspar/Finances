ef6a69ce86f3b4211f16b08b0d84ccd71073cb61
gngaspar <ggaspar@netcabo.pt>
2016-10-26 14:33:27 +0200

    Change configuration of Database and Added SqlGenerator

--- Finances.DataLayer/Migrations/Configuration.cs
+++ Finances.DataLayer/Migrations/Configuration.cs
@@ -1,28 +1,30 @@
 namespace Finances.DataLayer.Migrations
 {
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
-    
+    using System.Data.Entity.Migrations.Model;
+    using System.Data.Entity.SqlServer;
+
     public sealed class Configuration : DbMigrationsConfiguration<BankingDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationsEnabled = false;
+            this.SetSqlGenerator("System.Data.SqlClient", new CustomSqlServerMigrationSqlGenerator());
         }
 
         protected override void Seed(BankingDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            // This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            // You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            // to avoid creating duplicate seed data. E.g.
+            // context.People.AddOrUpdate(
+            // p => p.FullName,
+            // new Person { FullName = "Andrew Peters" },
+            // new Person { FullName = "Brice Lambson" },
+            // new Person { FullName = "Rowan Miller" }
+            // );
         }
     }
 }
