36dc304f53022fae37a04bb33595dcf7a1d2a3a5
gngaspar <ggaspar@netcabo.pt>
2016-10-26 14:32:06 +0200

    Added Account Entity configuration

---
+++ Finances.DataLayer/Configurations/AccountEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace Finances.DataLayer.Configurations
+{
+    using Domain.Banking;
+
+    public class AccountEntityConfiguration : EntityTypeConfiguration<AccountEntity>
+    {
+        public AccountEntityConfiguration()
+        {
+            this.HasKey(p => p.Code).ToTable("Accounts");
+            this.Property(p => p.Description).IsRequired().HasMaxLength(100);
+            this.Property(p => p.Amount).IsRequired().HasPrecision(18, 2);
+        }
+    }
+}
