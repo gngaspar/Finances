4f77e911f0e8ff80326381580b8b9d8eef9fb715
gngaspar <ggaspar@netcabo.pt>
2016-10-26 14:28:01 +0200

    Added Entity bases

---
+++ Finances.Domain/EntityDateTimeBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finances.Domain
+{
+    public abstract class EntityDateTimeBase
+    {
+        /// <summary>
+        /// Gets or sets the Created At.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Change At.
+        /// </summary>
+        public DateTime ChangeAt { get; set; }
+    }
+}
---
+++ Finances.Domain/EntityOwnerBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Finances.Domain
+{
+    using System;
+
+    using Finances.Domain.Banking;
+    using Finances.Domain.Human;
+
+    public abstract class EntityOwnerBase : EntityDateTimeBase
+    {
+        /// <summary>
+        /// Gets or sets the Code.
+        /// </summary>
+        public Guid Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Owner.
+        /// </summary>
+        public UserEntity Owner { get; set; }
+
+        /// <summary>
+        /// Gets or sets the the bank.
+        /// </summary>
+        public BankEntity Bank { get; set; }
+    }
+}
