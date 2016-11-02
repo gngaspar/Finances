2de79c1d819fadd320cd05bc9cb5d05fe3da60f0
gngaspar <ggaspar@netcabo.pt>
2016-11-02 01:45:16 +0100

    Added default data loans and savings

--- Finances.DataLayer/Migrations/Configuration.cs
+++ Finances.DataLayer/Migrations/Configuration.cs
@@ -2,6 +2,8 @@ namespace Finances.DataLayer.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Finances.Common.Banking;
     using Finances.DataLayer.Extension;
     using Finances.Domain;
     using Finances.Domain.Banking;
@@ -81,32 +83,49 @@ namespace Finances.DataLayer.Migrations
                 filipaPerson
             );
 
+            var currentGoncaloGuid = Guid.Parse("303B2432-C710-4DD6-A034-17EB10793CEB");
             var currentGoncalo = new CurrentAccountEntity
             {
-                Code = Guid.Parse("303B2432-C710-4DD6-A034-17EB10793CEB"),
+                Code = currentGoncaloGuid,
                 Description = "Conta ordenado",
                 Number = "1212",
                 Iban = "Pt12",
-                Currency = euro,
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
                 Amount = 1000.55m,
                 StartDate = DateTime.Today.AddYears(-5),
-                Holder = goncaloPerson,
-                Owner = goncaloPerson,
-                Bank = bankMontepio
+                Holder = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMontepioGuid)
             };
+
+            var currentPlGoncalo = new CurrentAccountEntity
+            {
+                Code = Guid.Parse("28244669-e675-4f28-bfeb-3074eb556c40"),
+                Description = "Conta Polonia",
+                Number = "1212",
+                Iban = "Pt12",
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Pln"),
+                Amount = 1000.55m,
+                StartDate = DateTime.Today.AddYears(-5),
+                Holder = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMilleniumGuid)
+            };
+
+            var currentGuiGuid = Guid.Parse("9D73C73A-7E1F-40A9-A52E-460010566B4F");
 
             var currentGuilherme = new CurrentAccountEntity
             {
-                Code = Guid.Parse("9D73C73A-7E1F-40A9-A52E-460010566B4F"),
+                Code = currentGuiGuid,
                 Description = "Conta fun",
                 Number = "121212",
                 Iban = "Pt12233",
-                Currency = euro,
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
                 Amount = 500.51m,
                 StartDate = DateTime.Today.AddYears(-2),
-                Holder = guilhermePerson,
-                Owner = goncaloPerson,
-                Bank = bankMontepio
+                Holder = context.Persons.SingleOrDefault(i => i.Code == guiGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMontepioGuid)
             };
 
             var currentVasco = new CurrentAccountEntity
@@ -115,12 +134,12 @@ namespace Finances.DataLayer.Migrations
                 Description = "Conta Corrente",
                 Number = "121212",
                 Iban = "Pt12233",
-                Currency = euro,
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
                 Amount = 0,
                 StartDate = DateTime.Today.AddYears(-2),
-                Holder = vascoPerson,
-                Owner = goncaloPerson,
-                Bank = bankCaixa
+                Holder = context.Persons.SingleOrDefault(i => i.Code == vascoGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankCaixaGuid)
             };
 
             var currentFilipa = new CurrentAccountEntity
@@ -129,19 +148,126 @@ namespace Finances.DataLayer.Migrations
                 Description = "Conta Corrente",
                 Number = "123232323",
                 Iban = "Pts3434343434",
-                Currency = euro,
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
                 Amount = 0m,
                 StartDate = DateTime.Today.AddYears(-2),
-                Holder = filipaPerson,
-                Owner = goncaloPerson,
-                Bank = bankBcp
+                Holder = context.Persons.SingleOrDefault(i => i.Code == filipaGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankBcpGuid)
+            };
+
+            var loanGoncalo = new LoanAccountEntity
+            {
+                Code = Guid.Parse("f03f8057-e33e-4ebd-8b50-7005d8bd2188"),
+                Description = "Conta Casa",
+                Number = "1212",
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
+                Amount = 1000.55m,
+                StartDate = DateTime.Today.AddYears(-5),
+                Holder = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMontepioGuid),
+                InicialAmount = 70000,
+                PremiumPercentage = 0,
+                InterestNetRate = 0.35m,
+                LoanEndDate = DateTime.Today.AddYears(15),
+                LoanInterestRate = 0.25m,
+                LoanRelatedAccount = (CurrentAccountEntity)context.Accounts.SingleOrDefault(i => i.Code == currentGoncaloGuid)
+            };
+
+            var loan2Goncalo = new LoanAccountEntity
+            {
+                Code = Guid.Parse("5e919e86-0d5a-4eca-b805-0a22471443cb"),
+                Description = "Conta Carro",
+                Number = "1212",
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
+                Amount = 1000.55m,
+                StartDate = DateTime.Today.AddYears(-5),
+                Holder = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMontepioGuid),
+                InicialAmount = 35000,
+                PremiumPercentage = 0,
+                InterestNetRate = 0.35m,
+                LoanEndDate = DateTime.Today.AddYears(15),
+                LoanInterestRate = 0.25m,
+                LoanRelatedAccount = (CurrentAccountEntity)context.Accounts.SingleOrDefault(i => i.Code == currentGoncaloGuid)
+            };
+
+            var savingGoncalo = new SavingAccountEntity
+            {
+                Code = Guid.Parse("ec7bdd2c-a3fe-42f0-9e91-b2e4333b9ae9"),
+                Description = "Poupanca",
+                Number = "1212",
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
+                Amount = 1000.55m,
+                StartDate = DateTime.Today.AddYears(-5),
+                Holder = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMontepioGuid),
+                AutomaticRenovation = AutomaticRenovation.YesSamePeriod,
+                InterestCapitalization = false,
+                InterestPayment = InterestPayment.Anual,
+                SavingEndDate = DateTime.Today.AddYears(5),
+                SavingInterestRate = 0.1m,
+                SavingRelatedAccount = (CurrentAccountEntity)context.Accounts.SingleOrDefault(i => i.Code == currentGoncaloGuid)
+            };
+
+            var savingGui = new SavingAccountEntity
+            {
+                Code = Guid.Parse("AA111116-70F2-47CC-9C21-01A161D10D92"),
+                Description = "Poupanca",
+                Number = "1212",
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
+                Amount = 400.00m,
+                StartDate = DateTime.Today.AddYears(-5),
+                Holder = context.Persons.SingleOrDefault(i => i.Code == guiGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMontepioGuid),
+                AutomaticRenovation = AutomaticRenovation.YesSamePeriod,
+                InterestCapitalization = false,
+                InterestPayment = InterestPayment.Anual,
+                SavingEndDate = DateTime.Today.AddYears(5),
+                SavingInterestRate = 0.1m,
+                SavingRelatedAccount = (CurrentAccountEntity)context.Accounts.SingleOrDefault(i => i.Code == currentGuiGuid)
+            };
+
+            var saving2Gui = new SavingAccountEntity
+            {
+                Code = Guid.Parse("7D2E570A-BBC3-41D6-B2E3-B47BB491D60C"),
+                Description = "Poupanca",
+                Number = "1212",
+                Currency = context.Currencies.SingleOrDefault(i => i.CodeName == "Euro"),
+                Amount = 250.00m,
+                StartDate = DateTime.Today.AddYears(-5),
+                Holder = context.Persons.SingleOrDefault(i => i.Code == guiGuid),
+                Owner = context.Persons.SingleOrDefault(i => i.Code == goncaloGuid),
+                Bank = context.Banks.SingleOrDefault(i => i.Code == bankMontepioGuid),
+                AutomaticRenovation = AutomaticRenovation.YesSamePeriod,
+                InterestCapitalization = false,
+                InterestPayment = InterestPayment.Anual,
+                SavingEndDate = DateTime.Today.AddYears(5),
+                SavingInterestRate = 0.1m,
+                SavingRelatedAccount = (CurrentAccountEntity)context.Accounts.SingleOrDefault(i => i.Code == currentGuiGuid)
             };
 
             context.SeedAddOrUpdate(p => p.Code, p => new { p.Description, p.Number, p.Iban, p.Amount, p.ChangeAt },
                currentGoncalo,
                currentGuilherme,
                currentVasco,
-               currentFilipa
+               currentFilipa,
+               currentPlGoncalo
+            );
+
+            context.SeedAddOrUpdate(p => p.Code, p => new { p.Description, p.Number, p.Amount, p.ChangeAt, p.InicialAmount, p.InterestNetRate, p.LoanEndDate, p.LoanInterestRate, p.PremiumPercentage, p.LoanRelatedAccount },
+               loanGoncalo,
+               loan2Goncalo
+            );
+
+            context.SeedAddOrUpdate(p => p.Code, p => new { p.Description, p.Number, p.Amount, p.ChangeAt, p.AutomaticRenovationString, p.InterestCapitalization, p.InterestPaymentString, p.SavingEndDate, p.SavingInterestRate, p.SavingRelatedAccount },
+               savingGoncalo,
+               savingGui,
+               saving2Gui
             );
         }
     }
