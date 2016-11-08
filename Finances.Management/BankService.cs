68041be9daa6e94dc64160bbc5b7ee9beeb88b37
gngaspar <ggaspar@netcabo.pt>
2016-11-08 19:34:59 +0100

    Change folder name and namespace

---
+++ Finances.Management/BankService.cs
@@ -0,0 +1,24 @@
+﻿namespace Finances.Management
+{
+    using System.Threading.Tasks;
+    using Finances.Contract.Banking;
+    using Finances.Domain;
+    using Finances.Domain.Repository;
+
+    public class BankService : IBankService
+    {
+        private readonly IBankRepository bankRepository;
+
+        public BankService(
+            IBankRepository bankRepository
+            )
+        {
+            this.bankRepository = bankRepository;
+        }
+
+        public async Task<BankListResponse> BankList(BankListRequest request)
+        {
+            return await this.bankRepository.GetList(request);
+        }
+    }
+}
