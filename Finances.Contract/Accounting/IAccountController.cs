27b53fc998532ec5511fd77dc6472bde33927cb1
gngaspar <ggaspar@netcabo.pt>
2017-05-19 17:17:36 +0200

    Add files via upload
    Change the interface to output a HttpMessega

--- Finances.Contract/Accounting/IAccountController.cs
+++ Finances.Contract/Accounting/IAccountController.cs
@@ -1,6 +1,7 @@
 ﻿namespace Finances.Contract.Accounting
 {
     using System;
+    using System.Net.Http;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -20,18 +21,6 @@
         /// <returns>
         /// The <see cref="Task"/>.
         /// </returns>
-        Task<ActionResponse<AccountListResponse>> List(Guid owner, AccountListRequest input);
-
-        Task<ActionResponse<CurrentAccountOut>> GetCurrentAccount(Guid owner, Guid account);
-
-        Task<ActionResponse<LoanAccountOut>> GetLoanAccount(Guid owner, Guid account);
-
-        Task<ActionResponse<SavingAccountOut>> GetSavingAccount(Guid owner, Guid account);
-
-        Task<ActionResponse<Guid>> AddCurrentAccount(Guid owner, CurrentAccountIn input);
-
-        Task<ActionResponse<Guid>> AddSavingAccount(Guid owner, Guid currentAccount, SavingAccountIn input);
-
-        Task<ActionResponse<Guid>> AddLoanAccount(Guid owner, Guid currentAccount, LoanAccountIn input);
+        Task<HttpResponseMessage> List(Guid owner, AccountListRequest input);
     }
 }
