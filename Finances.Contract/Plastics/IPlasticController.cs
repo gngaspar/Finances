90f9ef3a935114d9762e3d92c6636a5afabdfd2d
gngaspar <ggaspar@netcabo.pt>
2017-10-08 22:06:15 +0200

    Add the controller service call

--- Finances.Contract/Plastics/IPlasticController.cs
+++ Finances.Contract/Plastics/IPlasticController.cs
@@ -9,10 +9,27 @@
 
 namespace Finances.Contract.Plastics
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
     /// <summary>
     /// The PlasticController interface.
     /// </summary>
     public interface IPlasticController
     {
+        /// <summary>
+        /// The list.
+        /// </summary>
+        /// <param name="owner">
+        /// The owner guid.
+        /// </param>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        Task<HttpResponseMessage> List( Guid owner, CardListRequest input );
     }
 }
--- Finances.Endpoint.WebApi/ApiControllers/PlasticController.cs
+++ Finances.Endpoint.WebApi/ApiControllers/PlasticController.cs
@@ -9,15 +9,51 @@
 
 namespace Finances.Endpoint.WebApi.ApiControllers
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
     using System.Web.Http;
+    using System.Web.Http.Description;
 
+    using Finances.Contract;
     using Finances.Contract.Plastics;
+    using Finances.Domain;
+    using Finances.Domain.Wrappers;
 
     /// <summary>
     /// The Plastic Controller.
     /// </summary>
     [RoutePrefix( "Plastic" )]
-    public class PlasticController : ApiController, IPlasticController
+    public class PlasticController : BaseController, IPlasticController
     {
+        /// <summary>
+        /// The service.
+        /// </summary>
+        private readonly IPlasticService plasticService;
+
+        public PlasticController( IPlasticService plasticService )
+        {
+            this.plasticService = plasticService;
+        }
+
+        /// <summary>
+        /// The list.
+        /// </summary>
+        /// <param name="owner">
+        /// The owner for example 9B8B32D1-A950-4C11-B77D-6FEFFAA4C17B
+        /// </param>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        [HttpPost]
+        [Route( "{owner:guid}/List" )]
+        [ResponseType( typeof( ActionResponse<CardListResponse> ) )]
+        public async Task<HttpResponseMessage> List( Guid owner, CardListRequest input )
+        {
+            return await this.ProcessActionAsync( owner, input, this.plasticService.List );
+        }
     }
 }
