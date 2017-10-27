1fa681cd401eb3e41b35ce411f1270551e2c75ff
gngaspar <ggaspar@netcabo.pt>
2017-10-27 21:31:34 +0200

    Started to add frontend

---
+++ Finances.Endpoint.Frontend/Controllers/FaqController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Finances.Endpoint.Frontend.Controllers
+{
+    using System.Web.Mvc;
+
+    public class FaqController : Controller
+    {
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
